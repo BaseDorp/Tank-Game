1f9bd04dba00f4335a6b02f523317d042dc398d5
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-02-12 09:31:35 -0600

    Initial Commit

---
+++ TankGame/Assets/Scripts/PlayerTank.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTank : Tank
+{
+    float horizontalValue = 0;
+    float verticalValue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void UpdateMovement()
+    {
+        horizontalValue = Input.GetAxis("Horizontal");
+        verticalValue = Input.GetAxis("Vertical");
+
+        if (horizontalValue > 0)
+        {
+            // Move Up
+            Debug.Log("Move Up");
+            this.tankRB.AddForce(this.tankTransform.forward * this.movementSpeed);
+        }
+        else if (horizontalValue < 0)
+        {
+            // Move Down
+        }
+
+        if (verticalValue > 0)
+        {
+            // Move Right
+        }
+        else if (verticalValue < 0)
+        {
+            // Move Left
+        }
+    }
+}
---
+++ TankGame/Assets/Scripts/Tank.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    protected Transform tankTransform;
+    protected Rigidbody tankRB;
+
+    [SerializeField]
+    protected float movementSpeed;
+    [SerializeField]
+    float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tankTransform = GetComponent<Transform>();
+        tankRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
